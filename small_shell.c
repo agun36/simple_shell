2ecfb2812e83dc6868e92a92952f71cf4af12626
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-19 19:04:15 +0100

    Update small_shell.c

--- small_shell.c
+++ small_shell.c
@@ -92,8 +92,7 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 	}
 	if (is_pipe == 0)
 		_puts("\n");
-	
 	free_env(vars.envi);
-	free(vars.buff);
+	free(vars.buf);
 	exit(vars.stat);
 }
