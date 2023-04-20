562cc2792dd9c0c773ff56d93195d92749f97072
Odegua Osajele <odeguaj@gmail.com>
2023-04-20 04:49:04 -0700

    simple_shell

--- builtins.c
+++ builtins.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * add_keys - this adds a new environment variables
+ * add_key - this adds a new environment variables
  * @variables: pointer to the struct of variables
  *
  * Return: Nothing
@@ -9,67 +9,63 @@
 
 void add_key(vars_t *variables)
 {
-	unsigned int a;
+	unsigned int a = 0;
+	int i;
 	char **newenvironment;
 
-	for (a = 0; variables->environment[a] != NULL; a++)
-		;/* add new variable to the list of environ variables */
-
-	newenvironment = malloc(sizeof(char *) * (a + 3));
-	/*memory for the list of environ variables thats larger than the currentlist*/
-
-	if (!newenvironment)
+	while (variables->environment[a] != NULL)
 	{
-		print_error(variables, NULL);
-		variables->stat = 98;
-		new_exit(variables);
-	}
+		newenvironment = malloc(sizeof(char *) * (a + 3));
 
-	for (a = 0; variables->environment[a] != NULL; a++)
-		newenvironment[a] = variables->environment[a];
-	newenvironment[a] = add_value(variables->av[1], variables->av[2]);
-	if (!newenvironment[a])
-	{
-		print_error(variables, NULL);
-		free(varaiables->buf);
-		free(variables->commands);
-		free(variables->av);
-		free_env(variables->environment);
-		free(newenvironment);
-		exit(98);
+		for (int i = 0; i < a; i++)
+		{
+	/* add new variable to the list of environ variables */
+
+			newenvironment[i] = variables->environment[i];
+		}
+		newenvironment[a] = add_value(variables->av[1], variables->av[2]);
+		    a++;
 	}
 	newenvironment[a + 1] = NULL;
+
 	free(variables->environment);
-	variables->environment = newenv;
+	variables->environment = newenvironment;
+
 }
 
 /**
- * atoi - this converts a string into an integer
+ * _atoi - this converts a string into an integer
  * @string: tgis is the string to be convertes
  * Return: the converted integer
  */
 
-int atoi(char *string)
+int _atoi(char *string)
 {
 	int numbers = 0;
 	int check_num;
 	unsigned int a, bab;
 
 	check_num = Max_num;
+	a = 0;
+	bab = 0;
 
-	for  (a = 0; check_num != 0; bab++)
+	while (check_num != 0)
+	{
 		check_num = check_num / 10;
-	for (a = 0; string[a] != '\0' && a < bab; a++)
+		bab++;
+	}
+	while (string[a] != '\0' && a < bab)
 	{
 		numbers = numbers + string[a] - '0';
-		if ((string[a + 1] != '\0') && (numbers > Max_num / 10) && (a == bab - 2)
-				return (-1);
+		if ((string[a + 1] != '\0') && (numbers > Max_num / 10) && (a == bab - 2))
+			return (-1);
 		numbers = numbers * 10;
 		if ((string[a] - '0' > Max_num % 10) && (a == bab - 1))
 			return (-1);
 		if (string[a] < '0' && string[a] > 9)
 			return (-1);
-	}
+		a++;
+		}
 	if (a > bab)
 		return (-1);
 		return (numbers);
@@ -81,28 +77,35 @@ int atoi(char *string)
  * @value: the value of the variable
  * Return: the new string
  */
-
 char *add_value(char *name, char *value)
 {
 	char *key;
 	unsigned int length1, length2, a, b;
 
 	length1 = _strlen(name);
-	length2 = _strlen(vakue);
+	length2 = _strlen(value);
+
 	key = malloc(sizeof(char) * (length1 + length2 + 2));
 
 	if (!key)
 		return (NULL);
-	for (a = 0; name[a] != '\0'; a++)
+	a = 0;
+	while (name[a] != '\0')
+	{
 		key[a] = name[a];
-
+		a++;
+	}
 	key[a] = '=';
-	for (b = 0; value[b] != '\0'; b++)
+	b = 0;
+
+	while (value[b] != '\0')
+	{
 		key[b + a + 1] = value[b];
+		b++;
+	}
 	key[1 + b + a] = '\0';
 		return (key);
 }
-
 
 /**
  * find_key - this finds a variable
@@ -110,20 +113,24 @@ char *add_value(char *name, char *value)
  * @name: the environment variable to find
  * Return: address to the variable
  */
-
 char **find_key(char **environment, char *name)
 {
-	unsigned int a, b;
-	unsigned int length;
+	unsigned int a = 0;
+	unsigned int length = strlen(name);
 
-	length = strlen(name);
-	for (a = 0; environment[a] != '\0'; a++)
+	while (environment[a] != '\0')
 	{
-		for (b = 0; b < length; b++)
+		unsigned int b = 0;
+
+		while (b < length)
+		{
 			if (name[b] != environment[a][b])
 				break;
+				b++;
+		}
 		if (environment[a][b] == '=' && b == length)
 			return (&environment[a]);
+			a++;
 	}
 	return (NULL);
 }
