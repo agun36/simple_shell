31d2c1780f77a8cbdf1261d829ad2f7356905a59
Odegua Osajele <odeguaj@gmail.com>
2023-04-22 10:16:49 -0700

    simple shell

--- builtins0.c
+++ builtins0.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 /**
  * check_for_builtins - this checks for the command in the builtin variables
- * @variables: the variables
+ * @varial: the variables
  * Return: NULL
  */
-void (*check_for_builtins(vars_t *variables))(vars_t *variables)
+void (*check_for_builtins(vars_t *varial))(vars_t *varial)
 {
 	unsigned int a = 0;
 	builtins_t check[] = {
@@ -16,160 +16,160 @@ void (*check_for_builtins(vars_t *variables))(vars_t *variables)
 	};
 	while (check[a].f != NULL)
 	{
-		if (_strcmpr(variables->av[0], check[a].name) == 0)
+		if (_strcmpr(varial->av[0], check[a].name) == 0)
 			break;
 		a++;
 	}
 	if (check[a].f != NULL)
-		check[a].f(variables);
+		check[a].f(varial);
 	return (check[a].f);
 }
 
 /**
  * _env - this prints the current environment
- * @variables: the struct of variables
+ * @varial: the struct of variables
  * Return: Nothing
  */
 
-void _env(vars_t *variables)
+void _env(vars_t *varial)
 {
 	unsigned int a = 0;
 
-	while (variables->envi[a])
+	while (varial->envi[a])
 	{
-		_puts(variables->envi[a]);
+		_puts(varial->envi[a]);
 		_puts("\n");
 		a++;
 	}
-	variables->stat = 0;
+	varial->stat = 0;
 }
 
 /**
  * new_unsetenv - this removes an environment variable
- * @variables: variables
+ * @varial: variables
  * Return: Nothing
  */
 
-void new_unsetenv(vars_t *variables)
+void new_unsetenv(vars_t *varial)
 {
 	char **name, **environ;
 	unsigned int a, b;
 
-	if (variables->av[1] == NULL)
+	if (varial->av[1] == NULL)
 	{
-		print_error(variables, ": Incorrect\n");
-		variables->stat = 2;
+		print_error(varial, ": Incorrect\n");
+		varial->stat = 2;
 		return;
 	}
-	name = find_key(variables->envi, variables->av[1])
+	name = find_key(varial->envi, varial->av[1])
 	if (name == NULL)
 	{
-		print_error(variables, ": No variable");
+		print_error(varial, ": No variable");
 		return;
 	}
-	for (a = 0; variables->envi[a] != NULL; a++)
+	for (a = 0; varial->envi[a] != NULL; a++)
 		;
 	environ = malloc(sizeof(char *) * a);
 	if (environ == NULL)
 	{
-		print_error(variables, NULL);
-		variables->stat = 98;
-		new_exit(variables);
+		print_error(varial, NULL);
+		varial->stat = 98;
+		new_exit(varial);
 	}
 	a = 0;
-	while (variables->envi[a] != *name)
+	while (varial->envi[a] != *name)
 	{
-		environ[a] = variables->envi[a];
+		environ[a] = varial->envi[a];
 		a++; }
 	b = a + 1;
-	while (variables->envi[b] != NULL)
+	while (varial->envi[b] != NULL)
 	{
-		environ[a] = variables->envi[b];
+		environ[a] = varial->envi[b];
 			a++;
 			b++; }
 	environ[a] = NULL;
 	free(*name);
-	free(variables->envi);
-	variables->envi = environ;
-	variables->stat = 0;
+	free(varial->envi);
+	varial->envi = environ;
+	varial->stat = 0;
 }
 
 /**
  * new_exit - this exits the program
- * @variables: these are the variables
+ * @varial: these are the variables
  * Return: Nothing
  */
-void new_exit(vars_t *variables)
+void new_exit(vars_t *varial)
 {
 	int stat;
 	int a = 0;
 
-	while (_strcmpr(variables->av[a], "exit") == 0 && variables->av[a + 1] != NULL)
+	while (_strcmpr(varial->av[a], "exit") == 0 && varial->av[a + 1] != NULL)
 	{
-		stat = _atoi(variables->av[a + 1]);
+		stat = _atoi(varial->av[a + 1]);
 		if (stat == -1)
 		{
-			variables->stat = 2;
-			print_error(variables, ": Illicit number: ");
-			_puts2(variables->av[a + 1]);
+			varial->stat = 2;
+			print_error(varial, ": Illicit number: ");
+			_puts2(varial->av[a + 1]);
 			_puts2("\n");
-			free(variables->commands);
-			variables->commands = NULL;
+			free(varial->commands);
+			varial->commands = NULL;
 			return;
 		}
-		variables->stat = stat;
+		varial->stat = stat;
 		a++;
 	}
-	free(variables->buf);
-	free(variables->av);
-	free(variables->commands);
-	free_env(variables->envi);
-	exit(variables->stat);
+	free(varial->buf);
+	free(varial->av);
+	free(varial->commands);
+	free_env(varial->envi);
+	exit(varial->stat);
 }
 
 /**
  * new_setenv - this edits an existing variables
- * @variables: variables
+ * @varial: variables
  * Return: Nothing
  */
 
-void new_setenv(vars_t *variables)
+void new_setenv(vars_t *varial)
 {
 	char *variables;
 	char **name;
 
-	if (variables->av[2] == NULL)
+	if (varial->av[2] == NULL)
 	{
-		variables->stat = 2;
+		varial->stat = 2;
 		print_error(variables, "No correct number\n")
 		;
 	}
-	if (variables->av[1] == NULL)
+	if (varial->av[1] == NULL)
 	{
-		variables->stat == 2;
+		varial->stat == 2;
 		print_error(variables, "Error\n")
 		;
 	}
-	name = find_key(variables->envi,  variables->av[1]);
+	name = find_key(varial->envi,  varial->av[1]);
 	if (!name)
 		add_key(variables);
 
 	else
 	{
-		variables = add_value(variables->av[1], variables->av[2]);
-		if (!variable)
+		variables = add_value(varial->av[1], varial->av[2]);
+		if (!variables)
 		{
-			free(variables->buf);
-			free(variables->av);
-			free(variables->commands);
+			free(varial->buf);
+			free(varial->av);
+			free(varial->commands);
 			print_error(variables, NULL);
-			free_env(variables->envi);
+			free_env(varial->envi);
 			exit(98);
 		}
 		free(*name);
 		*name = variables;
 	}
-	variables->stat = 0;
+	varial->stat = 0;
 }
 
 
