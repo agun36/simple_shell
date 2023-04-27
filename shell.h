79a9517e4ea912521e5c4124e96a8a47f00be38e
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-27 14:30:25 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -198,12 +198,11 @@ int str_length(const char *string);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *str_concat(char *dest, char *src);
-char *_strdup(const char *str);
-char *_strdup(const char *str)
 char *str_duplicate(const char *string);
 void _puts(char *);
 void _memcpy(void *newptr, const void *ptr, unsigned int size);
 int _putchar(char);
 char *_strchr(char *, char);
-
+int bfree(void **);
+char *_strdup(void);:
 #endif
