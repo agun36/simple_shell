7fa069226e70929c5c13ddef24448b42413c7466
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-27 17:14:09 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -113,9 +113,9 @@ typedef struct builtin
 
 int is_interactive(info_t *info);
 int replace_string(char **old, char *new);
-int replace_vars(info_t *info)
+int replace_vars(info_t *)
 int replace_variables(info_t *info);
-int replace_alias(info_t *info);
+int replace_alias(info_t *);
 void check_chain_status(info_t *info, char *buf, size_t *current_position, size_t start_position, size_t buffer_length);
 int is_chain_delimiter(info_t *info, char *buf, size_t *current_position);
 char **strtow(char *, char *);
