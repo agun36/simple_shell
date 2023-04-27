8918e1c42c365ed3aa400e3f08b40f8e50d7be1c
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-27 21:38:14 +0100

    Update environment.c

--- environment.c
+++ environment.c
@@ -8,11 +8,6 @@
  */
 int print_environment(info_t *info)
 {
-	if (info == NULL)
-	{
-		fprintf(stderr, "Error: info is NULL\n");
-		return (1);
-	}
 	print_list_str(info->env);
 	return (0);
 }
@@ -28,18 +23,6 @@ char *get_env_var(info_t *info, const char *name)
 {
 	list_t *node = info->env;
 	char *p;
-
-	if (info == NULL)
-	{
-		fprintf(stderr, "Error: info is NULL\n");
-		return (NULL);
-	}
-
-	if (name == NULL)
-	{
-		fprintf(stderr, "Error: name is NULL\n");
-		return (NULL);
-	}
 
 	while (node)
 	{
@@ -60,12 +43,6 @@ char *get_env_var(info_t *info, const char *name)
  */
 int set_env_var(info_t *info)
 {
-	if (info == NULL)
-	{
-		fprintf(stderr, "Error: info is NULL\n");
-		return (1);
-	}
-
 	if (info->argc != 3)
 	{
 		_eputs("Incorrect number of arguements\n");
@@ -85,13 +62,7 @@ int set_env_var(info_t *info)
 int unset_env_var(info_t *info)
 {
 	int i;
-
-	if (info == NULL)
-	{
-		fprintf(stderr, "Error: info is NULL\n");
-		return (1);
-	}
-
+	
 	if (info->argc == 1)
 	{
 		_eputs("Too few arguements.\n");
@@ -113,13 +84,7 @@ int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
 	size_t i;
-
-	if (info == NULL)
-	{
-		fprintf(stderr, "Error: info is NULL\n");
-		return (1);
-	}
-
+	
 	for (i = 0; environ[i]; i++)
 		add_node_end(&node, environ[i], 0);
 	info->env = node;
