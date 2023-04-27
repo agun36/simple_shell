e391d5837579e6734a510fe31bfa8ca2f49fbf81
Odegua Osajele <odeguaj@gmail.com>
2023-04-27 16:57:16 -0700

    simple shell

--- list1.c
+++ list1.c
@@ -42,7 +42,7 @@ char **list_to_strings(list_t *head)
 
 	for (i = 0; node; node = node->next, i++)
 	{
-		str = malloc(str_length(node->str) + 1);
+		str = malloc(_strlen(node->str) + 1);
 		if (!str)
 		{
 			for (j = 0; j < i; j++)
