4b80de14331ec7b52014f796db6f55d754ca6d5a
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-27 08:20:50 +0100

    Update getline_func.c

--- getline_func.c
+++ getline_func.c
@@ -14,12 +14,7 @@ ssize_t input_buffer(info_t *info, char **buffer, size_t *buffer_length)
 	size_t length_param = 0;
 
 	if (!*buffer_length)
-<<<<<<< HEAD
-	{
-=======
 	{ 
-		// if nothing left in the buffer, fill it
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
 		free(*buffer);
 		*buffer = NULL;
 		signal(SIGINT, sigintHandler);
@@ -32,11 +27,8 @@ ssize_t input_buffer(info_t *info, char **buffer, size_t *buffer_length)
 		{
 			if ((*buffer)[read_bytes - 1] == '\n')
 			{
-<<<<<<< HEAD
 				(*buffer)[read_bytes - 1] = '\0'; 
-=======
 				(*buffer)[read_bytes - 1] = '\0';
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
 				read_bytes--;
 			}
 			info->linecount_flag = 1;
@@ -67,59 +59,45 @@ ssize_t get_input(info_t *info)
 	_putchar(BUF_FLUSH);
 	read_bytes = input_buf(info, &buffer, &buffer_length);
 	if (read_bytes == -1)
-<<<<<<< HEAD
 	{
-=======
-	{ 
-		/* EOF */
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
 		log_message("Reached end of file");
 		return (-1);
 	}
 	if (buffer_length)
 	{
-<<<<<<< HEAD
 		iterator = buffer_index;
 		pointer = buffer + buffer_index; 
 
 		check_chain(info, buffer, &iterator, buffer_index, buffer_length);
 		while (iterator < buffer_length)
 		{
-=======
-		/* we have commands left in the chain buffer */
-		iterator = buffer_index; /* init new iterator to current buffer position */
-		pointer = buffer + buffer_index; /* get pointer for return */
-		check_chain(info, buffer, &iterator, buffer_index, buffer_length);
-		while (iterator < buffer_length) 
-		{ 
-			/* iterate to semicolon or end */
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
-			if (is_chain(info, buffer, &iterator))
-				break;
-			iterator++;
+			/* we have commands left in the chain buffer */
+			iterator = buffer_index; /* init new iterator to current buffer position */
+			pointer = buffer + buffer_index; /* get pointer for return */
+			check_chain(info, buffer, &iterator, buffer_index, buffer_length);
+			while (iterator < buffer_length) 
+			{ 
+				/* iterate to semicolon or end */
+				if (is_chain(info, buffer, &iterator))
+					break;
+				iterator++;
+			}
+			buffer_index = iterator + 1;
+			if (buffer_index >= buffer_length)
+			{
+				buffer_index = iterator + 1; /* increment past nulled ';'' */
+				if (buffer_index >= buffer_length)
+				{
+					/* reached end of buffer? */
+					buffer_index = buffer_length = 0;
+					info->cmd_buf_type = CMD_NORM;
+				}
+				*buffer_pointer = pointer;
+				return (_strlen(pointer));
+			}
+			*buffer_pointer = buffer;
+			return (read_bytes);
 		}
-
-<<<<<<< HEAD
-		buffer_index = iterator + 1;
-		if (buffer_index >= buffer_length)
-		{
-=======
-		buffer_index = iterator + 1; /* increment past nulled ';'' */
-		if (buffer_index >= buffer_length)
-		{
-			/* reached end of buffer? */
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
-			buffer_index = buffer_length = 0;
-			info->cmd_buf_type = CMD_NORM;
-		}
-
-		*buffer_pointer = pointer;
-		return (_strlen(pointer));
-	}
-
-	*buffer_pointer = buffer;
-	return (read_bytes);
-}
 
 /**
  * read_buffer - reads a buffer
@@ -158,27 +136,22 @@ int get_line(info_t *info, char **buffer_ptr, size_t *buffer_length)
 	ssize_t read_status = 0, bf_st = 0;
 	char *pbf = NULL, *new_buffer = NULL, *new_line_char;
 
-<<<<<<< HEAD
 	pbf = *buffer_ptr;
 	if (pbf && buffer_length)
 		bf_st = *buffer_length;
 
 	if (buf_index == buf_len)
 		buf_index = buf_len = 0;
-
-=======
 	buffer = *buffer_ptr;
 	if (buffer && buffer_length)
 		buffer_length_status = *buffer_length;
 	if (buf_index == buf_len)
 		buf_index = buf_len = 0;
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
 	read_status = read_buffer(info, buf, &buf_len);
 	if (read_status == -1 || (read_status == 0 && buf_len == 0))
 		return (-1);
 
 	new_line_char = _strchr(buf + buf_index, '\n');
-<<<<<<< HEAD
 	line_len = c ? 1 + (unsigned int)(c - buf) : len;
 	new_p = _realloc(pbf, bf_st, bf_st ? bf_stat + in_len : line_len + 1);
 	if (!new_p)
@@ -186,54 +159,45 @@ int get_line(info_t *info, char **buffer_ptr, size_t *buffer_length)
 
 	if (!new_buffer)
 	{
-=======
-	line_len = new_line_char ? 1 + (unsigned int)(new_line_char - buf) : buf_len;
-	new_buffer = _realloc(buffer, buffer_length_status, buffer_length_status ? buffer_length_status + line_len : line_len + 1);
-	if (!new_buffer)
-	{ 
-		/* MALLOC FAILURE! */
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
-		fprintf(stderr, "Malloc failed!\n");
-		return (pbf ? free(pbf), -1 : -1);
-	}
-
-<<<<<<< HEAD
-	if (bf_st) 
-	{
-		_strncat(new_buffer, buf + buf_index, in_len - buf_index);
-	} 
-	else 
-	{
-		_strncpy(new_buffer, buf + buf_index, in_len - buf_index + 1);
-=======
-	if (buffer_length_status)
-	{
-		_strncat(new_buffer, buf + buf_index, line_len - buf_index);
-	} 
-	else
-	{
-		_strncpy(new_buffer, buf + buf_index, line_len - buf_index + 1);
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
-	}
-
-	bf_st += in_len - buf_index;
-	buf_index = in_len;
-	pbf = new_buffer;
-
-	if (buffer_length)
-<<<<<<< HEAD
-	{
-		*buffer_length = bf_st;
-		return;
-	}
-	*buffer_ptr = pbf;
-	return (bf_st);
-=======
-		*buffer_length = buffer_length_status;
-	*buffer_ptr = buffer;
-	return (buffer_length_status);
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
-}
+		line_len = new_line_char ? 1 + (unsigned int)(new_line_char - buf) : buf_len;
+		new_buffer = _realloc(buffer, buffer_length_status, buffer_length_status ? buffer_length_status + line_len : line_len + 1);
+		if (!new_buffer)
+		{ 
+			/* MALLOC FAILURE! */
+			fprintf(stderr, "Malloc failed!\n");
+			return (pbf ? free(pbf), -1 : -1);
+		}
+		
+		if (bf_st) 
+		{
+			_strncat(new_buffer, buf + buf_index, in_len - buf_index);
+		} 
+		else 
+		{
+			_strncpy(new_buffer, buf + buf_index, in_len - buf_index + 1);
+			if (buffer_length_status)
+			{
+				_strncat(new_buffer, buf + buf_index, line_len - buf_index);
+			} 
+			else
+			{
+				_strncpy(new_buffer, buf + buf_index, line_len - buf_index + 1);
+			}
+			bf_st += in_len - buf_index;
+			buf_index = in_len;
+			pbf = new_buffer;
+			
+			if (buffer_length)
+			{
+				*buffer_length = bf_st;
+				return;
+			}
+			*buffer_ptr = pbf;
+			return (bf_st);
+			*buffer_length = buffer_length_status;
+			*buffer_ptr = buffer;
+			return (buffer_length_status);
+		}
 
 /**
  * handle_sigint - Handles SIGINT signal (Ctrl+C)
@@ -243,13 +207,7 @@ int get_line(info_t *info, char **buffer_ptr, size_t *buffer_length)
  */
 void handle_sigint(int signal_number)
 {
-<<<<<<< HEAD
 	printf("\n");
 	printf("$ ");
 	fflush(stdout);
-=======
-    printf("\n");
-    printf("$ ");
-    fflush(stdout);
->>>>>>> 04d4340021aaa40824eaa7d9d3b5789c7973613f
 }
