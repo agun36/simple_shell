c0234cc685d72208def3b21991bd82bce7cff301
agun36 <aguntoroti10@gmail.com>
2023-04-26 23:47:52 -0700

    getline

--- environment.c
+++ environment.c
@@ -8,13 +8,13 @@
  */
 int print_environment(info_t *info)
 {
-	if (info == NULL) {
+	if (info == NULL)
+	{
 		fprintf(stderr, "Error: info is NULL\n");
-		return 1;
+		return (1);
 	}
-	
 	print_list_str(info->env);
-	return 0;
+	return (0);
 }
 
 /**
@@ -29,14 +29,16 @@ char *get_env_var(info_t *info, const char *name)
 	list_t *node = info->env;
 	char *p;
 
-	if (info == NULL) {
+	if (info == NULL)
+	{
 		fprintf(stderr, "Error: info is NULL\n");
-		return NULL;
+		return (NULL);
 	}
 
-	if (name == NULL) {
+	if (name == NULL)
+	{
 		fprintf(stderr, "Error: name is NULL\n");
-		return NULL;
+		return (NULL);
 	}
 
 	while (node)
@@ -58,19 +60,20 @@ char *get_env_var(info_t *info, const char *name)
  */
 int set_env_var(info_t *info)
 {
-	if (info == NULL) {
+	if (info == NULL)
+	{
 		fprintf(stderr, "Error: info is NULL\n");
-		return 1;
+		return (1);
 	}
 
 	if (info->argc != 3)
 	{
 		_eputs("Incorrect number of arguements\n");
-		return 1;
+		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
-		return 0;
-	return 1;
+		return (0);
+	return (1);
 }
 
 /**
@@ -83,20 +86,21 @@ int unset_env_var(info_t *info)
 {
 	int i;
 
-	if (info == NULL) {
+	if (info == NULL)
+	{
 		fprintf(stderr, "Error: info is NULL\n");
-		return 1;
+		return (1);
 	}
 
 	if (info->argc == 1)
 	{
 		_eputs("Too few arguements.\n");
-		return 1;
+		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
 		_unsetenv(info, info->argv[i]);
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -110,13 +114,14 @@ int populate_env_list(info_t *info)
 	list_t *node = NULL;
 	size_t i;
 
-	if (info == NULL) {
+	if (info == NULL)
+	{
 		fprintf(stderr, "Error: info is NULL\n");
-		return 1;
+		return (1);
 	}
 
 	for (i = 0; environ[i]; i++)
 		add_node_end(&node, environ[i], 0);
 	info->env = node;
-	return 0;
+	return (0);
 }
