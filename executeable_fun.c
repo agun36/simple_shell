009cc560896b22b0e091f5cbe000c66754b6fe1f
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-28 14:08:48 +0100

    Update executeable_fun.c

--- executeable_fun.c
+++ executeable_fun.c
@@ -36,7 +36,7 @@ char *_which(char *cmd, char **_environ)
 	struct stat st;
 
 	path_t = _getenv("PATH", _environ);
-	if (path)
+	if (path_t)
 	{
 		ptrPath = _strdup(path);
 		len_cmd = _strlen(cmd);
@@ -200,6 +200,6 @@ int cmd_exec(data_shell *datash)
 		} while (!WIFEXITED(s) && !WIFSIGNALED(s));
 	}
 
-	datash->s = s / 256;
+	datash->status = s / 256;
 	return (1);
 }
