37d53084a4bd8ae428be00b46d69c4868acce2d7
Agun Akindele Toroti <65425382+agun36@users.noreply.github.com>
2023-04-28 13:52:51 +0100

    Create get_env_fun.c

---
+++ get_env_fun.c
@@ -0,0 +1,183 @@
+#include "shell.h"
+
+/**
+ * check_envir - checks if the typed variable is an env variable
+ *
+ * @h: head of linked list
+ * @in: input string
+ * @data: data structure
+ * Return: no return
+ */
+void check_envir(r_var **h, char *in, data_shell *data)
+{ 
+	int row, chr, j, lval;
+	char **_envr;
+
+	_envr = data->_environ;
+	for (row = 0; _envr[row]; row++)
+	{
+		for (j = 1, chr = 0; _envr[row][chr]; chr++)
+		{
+			if (_envr[row][chr] == '=')
+			{
+				lval = _strlen(_envr[row] + chr + 1);
+				add_rvar_node(h, j, _envr[row] + chr + 1, lval);
+				return;
+			}
+
+			if (in[j] == _envr[row][chr])
+				j++;
+			else
+				break;
+		}
+	}
+
+	for (j = 0; in[j]; j++)
+	{
+		if (in[j] == ' ' || in[j] == '\t' || in[j] == ';' || in[j] == '\n')
+			break;
+	}
+
+	add_rvar_node(h, j, NULL, 0);
+}
+
+/**
+ * check_sign - check if the typed variable is $$ or $?
+ *
+ * @h: head of the linked list
+ * @in: input string
+ * @st: last status of the Shell
+ * @data: data structure
+ * Return: no return
+ */
+int check_sign(r_var **h, char *in, char *st, data_shell *data)
+{
+	int i, lst, lpd;
+
+	lst = _strlen(st);
+	lpd = _strlen(data->pid);
+
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '$')
+		{
+			if (in[i + 1] == '?')
+				add_rvar_node(h, 2, st, lst), i++;
+			else if (in[i + 1] == '$')
+				add_rvar_node(h, 2, data->pid, lpd), i++;
+			else if (in[i + 1] == '\n')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\0')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ' ')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == '\t')
+				add_rvar_node(h, 0, NULL, 0);
+			else if (in[i + 1] == ';')
+				add_rvar_node(h, 0, NULL, 0);
+			else
+				check_envir(h, in + i, data);
+		}
+	}
+
+	return (i);
+}
+
+/**
+ * replaced_input - replaces string into variables
+ *
+ * @head: head of the linked list
+ * @input: input string
+ * @new_input: new input string (replaced)
+ * @nlen: new length
+ * Return: replaced string
+ */
+char *replaced_input(r_var **head, char *input, char *new_input, int nlen)
+{
+	r_var *indx;
+	int i, j, k;
+
+	indx = *head;
+	for (j = i = 0; i < nlen; i++)
+	{
+		if (input[j] == '$')
+		{
+			if (!(indx->len_var) && !(indx->len_val))
+			{
+				new_input[i] = input[j];
+				j++;
+			}
+			else if (indx->len_var && !(indx->len_val))
+			{
+				for (k = 0; k < indx->len_var; k++)
+					j++;
+				i--;
+			}
+			else
+			{
+				for (k = 0; k < indx->len_val; k++)
+				{
+					new_input[i] = indx->val[k];
+					i++;
+				}
+				j += (indx->len_var);
+				i--;
+			}
+			indx = indx->next;
+		}
+		else
+		{
+			new_input[i] = input[j];
+			j++;
+		}
+	}
+
+	return (new_input);
+}
+
+/**
+ * rep_var - calls functions to replace string into vars
+ *
+ * @input: input string
+ * @datash: data structure
+ * Return: replaced string
+ */
+char *rep_var(char *input, data_shell *datash)
+{
+	r_var *head, *index;
+	char *status, *new_input;
+	int len1, len2;
+
+	status = _itoa(datash->status);
+	head = NULL;
+
+	 len1 = check_sign(&head, input, status, datash);
+
+	if (head == NULL)
+	{
+		free(status);
+		return (input);
+	}
+
+	index = head;
+	len2 = 0;
+
+	while (index != NULL)
+	{
+		len2 += (index->len_val - index->len_var);
+		index = index->next;
+	}
+
+	len2 +=  len1;
+
+	new_input = malloc(sizeof(char) * (len2 + 1));
+	new_input[len2] = '\0';
+
+	new_input = replaced_input(&head, input, new_input, nlen);
+
+	free(input);
+	free(status);
+	free_rvar_list(&head);
+
+	return (new_input);
+}
